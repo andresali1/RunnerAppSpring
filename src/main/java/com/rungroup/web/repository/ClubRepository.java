1d0b938e242449288642ef171da16117017500c4
Andrés Salinas <andresali1@hotmail.es>
2025-03-06 12:01:19 +0100

    Agregando primer modelo y repositorio

---
+++ src/main/java/com/rungroup/web/repository/ClubRepository.java
@@ -0,0 +1,11 @@
+package com.rungroup.web.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rungroup.web.models.Club;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+	Optional<Club> findByTittle(String title);
+}
